b902a608d0cd8c7691310915501931e5e2dc5dd8
mdbow22 <mdbow22@gmail.com>
2021-11-02 19:08:29 -0400

    added hook to user model for password hashing

--- models/users.js
+++ models/users.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 class User extends Model {};
 
@@ -22,12 +23,21 @@ User.init(
         password: {
             type: DataTypes.STRING,
             allowNull: false,
-            validate: {
-                len: [8]
-            }
         }
     },
     {
+        hooks: {
+            beforeCreate: async (newUserData) => {
+              newUserData.password = await bcrypt.hash(newUserData.password, 10);
+              return newUserData;
+            },
+            beforeUpdate: async (updatedUserData) => {
+                if (updatedUserData.password) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                }
+                return updatedUserData;
+              }
+        },
         sequelize,
         underscored: true,
         freezeTableName: true,
