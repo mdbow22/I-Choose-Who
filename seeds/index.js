44c2d4de6ac492db6954965fe696f9b0ab79874b
mdbow22 <mdbow22@gmail.com>
2021-11-02 22:01:05 -0400

    pokemon seed data added

--- seeds/index.js
+++ seeds/index.js
@@ -1,10 +1,12 @@
 const sequelize = require('../config/connection');
+const seedPokemon = require('./pokemon');
 const seedUsers = require('./users');
 
 const seedAll = async () => {
     await sequelize.sync({ force: true });
   
     await seedUsers();
+    await seedPokemon();
   
     process.exit(0);
   };
--- seeds/pokemon.js
+++ seeds/pokemon.js
@@ -1,4 +1,37 @@
 const { Pokemon } = require('../models');
-const releasedPokes = require('./released_pokemon.json');
+const allPokemon = require('./pokemon_types.json');
 
- 
+const pokemonSeeds = () => {
+    
+    let seedArray = [];
+    
+    for(let i = 0; i < allPokemon.length; i++) {
+        
+        let seedObj = {};
+
+        if(allPokemon[i].form === 'Normal' || allPokemon[i].form === 'Galarian' || allPokemon[i].form === 'Alola') {
+            seedObj.pokedex_id = allPokemon[i].pokemon_id;
+            seedObj.name = allPokemon[i].pokemon_name;
+
+            if(allPokemon[i].form === 'Galarian') {
+                seedObj.variant = 'galar';
+            } else if(allPokemon[i].form === 'Alola') {
+                seedObj.variant = 'alola'; 
+            }
+
+            seedArray.push(seedObj);
+        }
+    }
+
+    return seedArray;
+
+    };
+
+const seedPokemon = () => Pokemon.bulkCreate(pokemonSeeds());
+
+module.exports = seedPokemon;
+
+
+
+
+
