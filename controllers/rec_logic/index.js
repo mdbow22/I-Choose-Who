3b3e77108c37eee8e60fd8d132e11a70b89c9aec
mdbow22 <mdbow22@gmail.com>
2021-11-17 19:58:04 -0500

    reworked logic to prevent duplicates and changed logic for good cat

--- controllers/rec_logic/index.js
+++ controllers/rec_logic/index.js
@@ -52,57 +52,17 @@ const removeWeakPokemon = (userTeam, enemy) => {
 
 const getRecommendations = async (userTeam, enemy) => {
 
-    //Placeholder for call to database to fetch user's collection
+    //Get info about enemy team from our DB
     const enemyTeam = await team(enemy);
 
     //retrieve type information about enemy and user teams
-    await P.getTypeInfo(enemyTeam);
-    await P.getTypeInfo(userTeam);
-
-    /**
-     * Iterate over enemy team:
-     * 
-     * If enemyMon has immunity = exclude all in collection that are that single type
-     * 
-     * If enemyMon has weak_to4x category, find all pokemon in collection:
-     *  Best pokemon = 2 4xWeak types or 4xWeak
-     *  Better pokemon = 2 weak types
-     *  Good pokemon = no resistances
-     * 
-     * 
-     * If enemyMon doesn't have weak_to4x category:
-     *  best = 2 weak types
-     *  better = 1 weak
-     *  good = no resistances
-     * 
-     * Alternative Points system:
-     * 4x weak type = 2 points
-     * weak type = 1 point
-     * not resisted = 0 points
-     * resisted = -1 points
-     * immune = -2 points
-     * immune-to-enemy = 1 point
-     * 
-     * If list of recommendations empty: "You have no pokemon for this battle"
-     */
+    await Promise.all([P.getTypeInfo(enemyTeam), P.getTypeInfo(userTeam)]);
     
     for(const pokemon of enemyTeam) {
         pokemon.best = [];
         pokemon.better = [];
         pokemon.good = [];
 
-        /**
-         * New Logic:
-         * Eliminate pokemon 3 types of pokemon - 
-         * 1. Pokemon that have no effect on enemy
-         * 2. Pokemon that are weak to enemy
-         */
-
-        /**
-         * Original Recommendation logic:
-         * Issues: iterates through entire collection,
-         * doesn't account for pokemon that are weak to the enemy pokemon
-         */
 
         //build rec_team array of pokemon suitable for recommendation
         rec_team = [];
@@ -123,32 +83,35 @@ const getRecommendations = async (userTeam, enemy) => {
             //options if pokemon has 4xweak types
             if(pokemon.weak_to4x.length) {
 
-                //best options
+                //best options -> pokemon with at least 1 type that's 4x effective against enemy
                 if(pokemon.weak_to4x.includes(userMon.types[0]) || pokemon.weak_to4x.includes(userMon.types[1])) {
                     pokemon.best.push(userMon);
                 }
-                /* pokemon.weak_to4x.forEach((type) => {
-                    if(userMon.types.includes(type)) {
-                        pokemon.best.push(userMon);
-                    }
-                }); */
-
-                //better options
-                if(pokemon.weak_to.includes(userMon.types[0]) && pokemon.weak_to.includes(userMon.types[1])) {
+                //better options -> pokemon with at least 1 type that's 2x effective against enemy
+                else if(pokemon.weak_to.includes(userMon.types[0]) || pokemon.weak_to.includes(userMon.types[1])) {
                     pokemon.better.push(userMon);
-                //good options
-                } else if (pokemon.weak_to.includes(userMon.types[0]) || pokemon.weak_to.includes(userMon.types[1])) {
-                    pokemon.good.push(userMon);
+                //good options -> pokemon that the enemy at least doesn't resist
+                } else if (userMon.types.length === 2) {
+                    //good options if double type
+                    if(!pokemon.resists.includes(userMon.types[0]) && !pokemon.resists.includes(userMon.types[1])) {
+                        pokemon.good.push(userMon);
+                    }
+                } else {
+                    //good options if single type
+                    if(!pokemon.resists.includes(userMon.types[0])) {
+                        pokemon.good.push(userMon);
+                    }
                 }
 
             } else {
-                //best options if no weak_to4x
+                //best options if no weak_to4x -> both types 2x effective against enemy
+                //Revisit: odd choice that only dual-types can be "best" against single-type enemies
                 if(pokemon.weak_to.includes(userMon.types[0]) && pokemon.weak_to.includes(userMon.types[1])) {
                     pokemon.best.push(userMon);
-                //better options
+                //better options -> at least one type 2x effective against enemy
                 } else if (pokemon.weak_to.includes(userMon.types[0]) || pokemon.weak_to.includes(userMon.types[1])) {
                     pokemon.better.push(userMon);
-                //good options
+                //good options -> pokemon that the enemy at least doesn't resist
                 } else if (userMon.types.length === 2) {
                     //good options if double type
                     if(!pokemon.resists.includes(userMon.types[0]) && !pokemon.resists.includes(userMon.types[1])) {
